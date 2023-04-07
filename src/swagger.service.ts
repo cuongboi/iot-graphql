80dd25983f2c6f69a707671ace6b1792f978d43c
Cuong Nguyen <herculesboi@gmail.com>
2023-04-07 11:49:35 +0700

    remove: custom header config

--- src/swagger.service.ts
+++ src/swagger.service.ts
@@ -4,14 +4,12 @@ import SwaggerApi from 'swagger-to-service';
 
 @Injectable()
 export class SwaggerService extends SwaggerApi {
-  constructor(options?: Partial<AxiosRequestConfig>) {
+  constructor() {
     const axiosInstance: AxiosInstance = axios.create({
       baseURL: process.env.API_URL,
-      ...options,
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + process.env.API_TOKEN, 
-        ...options?.headers,
+        'Authorization': 'Bearer ' + process.env.API_TOKEN
       },
     });
 
